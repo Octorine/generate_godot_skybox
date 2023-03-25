7b0875517d1e981abd552e31e6f80490720d3320
James Jackson <j.e.m.jackson@gmail.com>
2023-03-25 10:27:13 -0500

    Use image crate instead of png.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,19 @@
 fn main() {
     use perlin_noise::PerlinNoise;
-    use png::Encoder;
-    use std::fs::File;
-    use std::io::BufWriter;
+    use image::ImageBuffer;
+
     let width = 4096;
     let height = 4096;
-    let file = File::create("test.png").unwrap();
-    let writer = BufWriter::new(file);
-    let mut png = Encoder::new(writer, width, height);
+
     let noise = PerlinNoise::new();
     let noise_ref = &noise;
-    png.set_color(png::ColorType::Grayscale);
-    let mut writer = png.write_header().unwrap();
 
-    let data: Vec<u8> = (0..width)
-        
-        .flat_map(|i| {
-            (0..height).map(move |j| {
-                let ifloat = i as f64;
-                let jfloat = j as f64;
-                let ftotal = noise_ref.get2d([ifloat / 256.0, jfloat / 256.0]);
-                (ftotal * 256.0).floor() as u8
-            })
-        })
-        .collect();
+    let buffer = ImageBuffer::from_fn(height, width, |x, y| {
+                let xfloat = x as f64;
+                let yfloat = y as f64;
+                let ftotal = noise_ref.get2d([xfloat / 256.0, yfloat / 256.0]);
+                image::Luma([(ftotal * 256.0).floor() as u8])
+    });
 
-    writer.write_image_data(&data).unwrap();
+    buffer.save("skymap.png").unwrap();
 }
