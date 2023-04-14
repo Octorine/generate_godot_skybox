2282db3df8e071b7b26bb7d17476fab0c7cf6af5
James Jackson <j.e.m.jackson@gmail.com>
2023-04-13 22:08:09 -0500

    Try to avoid distortion by sampling from a 3D perlin noise using spherical coordinates.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::f64::consts::PI;
+
 fn main() {
-    use perlin_noise::PerlinNoise;
     use image::ImageBuffer;
+    use perlin_noise::PerlinNoise;
 
     let width = 4096;
     let height = 4096;
@@ -8,12 +10,19 @@ fn main() {
     let noise = PerlinNoise::new();
     let noise_ref = &noise;
 
-    let buffer = ImageBuffer::from_fn(height, width, |x, y| {
-                let xfloat = x as f64;
-                let yfloat = y as f64;
-                let ftotal = noise_ref.get2d([xfloat / 256.0, yfloat / 256.0]);
-                image::Luma([(ftotal * 256.0).floor() as u8])
-    });
+    let buffer = ImageBuffer::from_fn(width, height, |x, y| {
+        let widthfloat = width as f64;
+        let heightfloat = height as f64;
+        let theta = (x as f64 / widthfloat - 0.5) * PI * 2.0;
+        let phi = (y as f64 / heightfloat - 0.5) * PI * 2.0;
+        let rho = 1.0;
+        let xfloat = rho * theta.sin() * phi.cos();
+        let zfloat = rho * theta.sin() * phi.sin();
+        let yfloat = rho * theta.cos();
 
+        let ftotal = noise_ref.get3d([xfloat, yfloat, zfloat]);
+
+        image::Luma([(ftotal * 255.0).floor() as u8])
+    });
     buffer.save("skymap.png").unwrap();
 }
