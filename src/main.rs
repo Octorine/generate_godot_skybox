4e6d7b2b1e56a5ee29fdeca7dbec83b8b5e52705
James Jackson <j.e.m.jackson@gmail.com>
2023-04-16 15:20:31 -0500

    Extract out spherical to cartesian coordinate conversion into a function.

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,27 @@ fn main() {
         let theta = (x as f64 / widthfloat - 0.5) * 2.0 * PI;
         let phi = (y as f64 / heightfloat - 0.5) * PI;
         let rho = 2.0;
-        let xfloat = rho * theta.cos() * phi.cos();
-        let zfloat = rho * theta.sin() * phi.cos();
-        let yfloat = rho * phi.cos();
-        let ftotal = noise_ref.get([xfloat, yfloat, zfloat]);
-        blend_pixel(ftotal, light, dark)
+        let total = noise_ref.get(sphere_to_cart(theta, phi, rho));
+        blend_pixel(total, light, dark)
     });
     buffer.save("skymap.png").unwrap();
 }
+
+fn sphere_to_cart(theta: f64, phi: f64, rho: f64) -> [f64; 3]{
+        let x = rho * theta.cos() * phi.cos();
+        let z = rho * theta.sin() * phi.cos();
+        let y = rho * phi.cos();
+        [x,  y, z]
+}
+
 fn blend_pixel(mix: f64, c1: Rgb<u8>, c2: Rgb<u8>) -> Rgb<u8> {
         Rgb([blend_element(mix, c1.0[0], c2.0[0]),
         blend_element(mix, c1.0[1], c2.0[1]),
         blend_element(mix, c1.0[2], c2.0[2])])
 }
+
 fn blend_element(mix: f64, e1: u8, e2: u8) -> u8 {
             let e1 = e1 as f64;
             let e2 = e2 as f64;
             (e1 * mix + e2 * (1.0 - mix)) as u8
 }
-
