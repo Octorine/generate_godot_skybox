93686d3c7f8d356ffb19b85121e7d1cb63fee541
James Jackson <j.e.m.jackson@gmail.com>
2023-04-16 09:14:42 -0500

    Fix distortion, use different noise, add color.
    * Godot expects horizontal and vertical resolution to be different.
      Change polar coordinates accordingly, to get rid of disotrtion at the
      poles.
    * Use noise crate instead of perlin_noise.  noise supports differnt
      noise types, including OpenSimplex.
    * Instead of grayscale, have our noise be a blend of two rgb colors.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
 use std::f64::consts::PI;
 
+use image::Rgb;
+
 fn main() {
     use image::ImageBuffer;
-    use perlin_noise::PerlinNoise;
-
+    use noise::{NoiseFn, OpenSimplex};
     let width = 4096;
     let height = 4096;
-
-    let noise = PerlinNoise::new();
+    let noise = OpenSimplex::new(1);
     let noise_ref = &noise;
-
+    let dark = Rgb([18, 18, 38]);
+    let light = Rgb([49, 50, 74]);
     let buffer = ImageBuffer::from_fn(width, height, |x, y| {
         let widthfloat = width as f64;
         let heightfloat = height as f64;
-        let theta = (x as f64 / widthfloat - 0.5) * PI * 2.0;
-        let phi = (y as f64 / heightfloat - 0.5) * PI * 2.0;
-        let rho = 1.0;
-        let xfloat = rho * theta.sin() * phi.cos();
-        let zfloat = rho * theta.sin() * phi.sin();
-        let yfloat = rho * theta.cos();
-
-        let ftotal = noise_ref.get3d([xfloat, yfloat, zfloat]);
-
-        image::Luma([(ftotal * 255.0).floor() as u8])
+        let theta = (x as f64 / widthfloat - 0.5) * 2.0 * PI;
+        let phi = (y as f64 / heightfloat - 0.5) * PI;
+        let rho = 2.0;
+        let xfloat = rho * theta.cos() * phi.cos();
+        let zfloat = rho * theta.sin() * phi.cos();
+        let yfloat = rho * phi.cos();
+        let ftotal = noise_ref.get([xfloat, yfloat, zfloat]);
+        blend_pixel(ftotal, light, dark)
     });
     buffer.save("skymap.png").unwrap();
 }
+fn blend_pixel(mix: f64, c1: Rgb<u8>, c2: Rgb<u8>) -> Rgb<u8> {
+        Rgb([blend_element(mix, c1.0[0], c2.0[0]),
+        blend_element(mix, c1.0[1], c2.0[1]),
+        blend_element(mix, c1.0[2], c2.0[2])])
+}
+fn blend_element(mix: f64, e1: u8, e2: u8) -> u8 {
+            let e1 = e1 as f64;
+            let e2 = e2 as f64;
+            (e1 * mix + e2 * (1.0 - mix)) as u8
+}
+
